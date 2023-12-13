dc75cd2019cad68bdbaa29c973237682ce4254d5
Peter Ross <pross@xvid.org>
2023-12-13 14:25:18 +1100

    benchmark

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Plain C implementataion of the Kaldi online feature extractor using using [FFTW](https://www.fftw.org/).
 
 Produces identical output to [kaldifeat](https://github.com/csukuangfj/kaldifeat) and [kaldi-native-fbank](https://github.com/csukuangfj/kaldi-native-fbank).
+
+## Benchmark
+
+See `bench.py`. Wall-clock time to compute features for 1000 second waveform:
+
+Implementation | Time (Seconds)
+---|---
+kaldifeat | 10.6698
+kali-native-feat | 1.5029
+knf | 0.3258
---
+++ bench.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import kaldifeat
+import kaldi_native_fbank as knf
+from ctypes import *
+import time
+import torch
+
+c_float_p = POINTER(c_float)
+
+knfc = simutil = CDLL("libknf.so")
+knfc.knf_create.argtypes = [c_int, c_int]
+knfc.knf_create.restype = c_void_p
+knfc.knf_accept_waveform.argtypes = [c_void_p, c_int, c_float_p, c_int]
+knfc.knf_num_frames_ready.argtypes = [c_void_p]
+knfc.knf_get_frame.argtypes = [c_void_p, c_int]
+knfc.knf_get_frame.restype = c_float_p
+
+def main():
+    sampling_rate = 16000
+    samples = torch.randn(16000 * 1000)
+    samples.numpy()
+
+    # kaldi feat
+
+    opts = kaldifeat.FbankOptions()
+    opts.frame_opts.dither = 0
+    opts.mel_opts.num_bins = 80
+    opts.frame_opts.snip_edges = False
+    opts.mel_opts.debug_mel = False
+
+    online_fbank = kaldifeat.OnlineFbank(opts)
+    start = time.time()
+    online_fbank.accept_waveform(sampling_rate, samples)
+    end = time.time()
+    print("kalifeat", end - start)
+
+    # knf
+
+    opts = knf.FbankOptions()
+    opts.frame_opts.dither = 0
+    opts.mel_opts.num_bins = 80
+    opts.frame_opts.snip_edges = False
+    opts.mel_opts.debug_mel = False
+
+    fbank = knf.OnlineFbank(opts)
+    start = time.time()
+    fbank.accept_waveform(sampling_rate, samples.tolist())
+    end = time.time()
+    print("kali-native-feat", end - start)
+
+    # knfc
+
+    k = knfc.knf_create(sampling_rate, 80)
+    start = time.time()
+    samples_ptr = samples.numpy().ctypes.data_as(c_float_p)
+    knfc.knf_accept_waveform(k, sampling_rate, samples_ptr, len(samples))
+    end = time.time()
+    print("knfc", end - start)
+
+if __name__ == "__main__":
+    torch.manual_seed(20220825)
+    main()
+
