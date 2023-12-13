b009213cdec023803716e413fa6b44e086718636
Peter Ross <pross@xvid.org>
2023-12-13 17:52:14 +1100

    c benchmark

--- README.md
+++ README.md
@@ -2,7 +2,16 @@ Plain C implementataion of the Kaldi online feature extractor using [FFTW](https
 
 Produces identical output to [kaldifeat](https://github.com/csukuangfj/kaldifeat) and [kaldi-native-fbank](https://github.com/csukuangfj/kaldi-native-fbank).
 
-## Benchmark
+## C Benchmark
+
+See `bench.cpp`. Wall-clock time to compute features for 1000 second waveform, averaged over 10 runs.
+
+Implementation | Time (Seconds)
+---|---
+kali-native-fbank | 0.4978459
+knf | 0.3398971
+
+## Python Benchmark
 
 See `bench.py`. Wall-clock time to compute features for 1000 second waveform:
 
---
+++ bench.cpp
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "kaldi-native-fbank/csrc/online-feature.h"
+#include "knf.h"
+
+int main()
+{
+    knf::FbankOptions opts;
+    opts.frame_opts.dither = 0;
+    opts.frame_opts.snip_edges = false;
+    opts.frame_opts.samp_freq = 16000;
+    opts.mel_opts.num_bins = 80;
+    knf::OnlineGenericBaseFeature<knf::FbankComputer> k1(opts);
+
+    KNF * k2 = knf_create(16000, 80);
+
+    srand48(1337);
+#define N (16000*1000)
+    float * samples = (float *)malloc(N * sizeof(float));
+    assert(samples);
+    struct timespec start, end;
+
+    for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < N; i++)
+            samples[i] = drand48();
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        k1.AcceptWaveform(16000, samples, N);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        double start1 = start.tv_sec + start.tv_nsec/1000000000.0;
+        double end1 = end.tv_sec + end.tv_nsec/1000000000.0;
+ 
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        knf_accept_waveform(k2, 16000, samples, N);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        double start2 = start.tv_sec + start.tv_nsec/1000000000.0;
+        double end2 = end.tv_sec + end.tv_nsec/1000000000.0;
+
+        printf("%f, %f\n", end1 - start1, end2 - start2);
+
+        k1.Pop(N);
+        knf_pop(k2, N);
+    }
+
+    knf_destroy(k2);
+}
